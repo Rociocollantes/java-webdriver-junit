811afb40ddf9a2997ead6d67005cad1c40b5bfc8
Deliana8 <katyarina.lobanova@gmail.com>
2019-12-05 21:26:27 -0800

    labanava: data file introduction

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -10,12 +10,16 @@ import static support.TestContext.getDriver;
 
 public class WebDriverTest {
 
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         TestContext.initialize();
-        getDriver().manage().deleteAllCookies();
         getDriver().manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Before
+    public void before() {
+        getDriver().manage().deleteAllCookies();
     }
 
     @Test
@@ -43,8 +47,8 @@ public class WebDriverTest {
         assertThat(actualDocuments).contains("Document 2");
     }
 
-    @After
-    public void teardown() {
+    @AfterClass
+    public static void teardown() {
         getDriver().quit();
     }
 
